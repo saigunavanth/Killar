444cbea931a09f367aee63470bbc2da5a006afb2
StarMan <saigunavanth11@gmail.com>
2022-06-21 04:59:10 +0530

    change

--- killar/settings.py
+++ killar/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 
 
+STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,"static") ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
